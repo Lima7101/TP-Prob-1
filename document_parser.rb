369f1ab18dfcf191259892bb26d573704a18f285
Pedro <flima0796@me.com>
2014-05-06 15:37:31 -0400

    removed comments

--- document_parser.rb
+++ document_parser.rb
@@ -4,13 +4,12 @@ include Treat::Core::DSL
 
 StanfordCoreNLP::Config::Models[:ner][:english] = 'muc.7class.distsim.crf.ser.gz'
 
-#d = document './../../corpora/LDC2013E64_DEFT_Phase_1_ERE_Annotation_R3_V2/data/source/events_workshop/EVENTS_WKSP_37_1.txt'
-d = document './../../corpora/trimmer/events.txt'
+
+d = document '.events.txt'
 
 
 d.do(:chunk, :segment, :tokenize, :parse, :name_tag)
-#d.do(:chunk, :segment, :tokenize, :parse, :hypernyms, :synonyms, :name_tag)
-#d.do(:chunk, :segment, :tokenize, :parse, :name_tag)
+
 
 d.children.each_with_index do |c, i|
   c.visualize :dot, file: "./../../output/simple/#{i+1}.dot"
